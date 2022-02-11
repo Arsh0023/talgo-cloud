c54a41d2f95c5d658d79c025c4d0820be7ee7e8e
Arshpreet <arsharsh2391@gmail.com>
2022-02-11 17:55:53 +0530

    initialized

---
+++ dry_run.py
@@ -0,0 +1,10 @@
+from py5paisa import FivePaisaClient
+from . import inf
+cred={
+    "APP_NAME": inf.APP_NAME,
+    "APP_SOURCE": inf.APP_SOURCE,
+    "USER_ID": inf.USER_ID,
+    "PASSWORD": inf.PASSWORD,
+    "USER_KEY": inf.USER_KEY,
+    "ENCRYPTION_KEY": inf.ENCRYPTION_KEY,
+    }
